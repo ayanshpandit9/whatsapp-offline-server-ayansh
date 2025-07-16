e315cd6670d5741fd94264e2b4f7e678d68ceb07
ayanshpandit9 <kartikmishra6803@gmail.com>
2025-07-16 13:56:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import subprocess
-import os
 
 app = Flask(__name__)
-pair_code_visible = False
+started = False
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-    if request.method == "POST":
-        phone_number = request.form.get("number")
-        if phone_number:
-            subprocess.Popen(["node", "bot.js"])
-            return render_template("pair.html", number=phone_number, pair_code=True)
-    return render_template("index.html")
+    global started
+    if not started:
+        subprocess.Popen(["node", "bot.js"])
+        started = True
+    return render_template("index.html", pair_code=True)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
